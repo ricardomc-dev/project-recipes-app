c6f86357752fe0b950493229354da6ba2135bf5a
Ricardo Carvalho <88890084+ricardomc-dev@users.noreply.github.com>
2022-02-04 19:16:06 -0300

    Update ApiFoods.js

--- src/service/ApiFoods.js
+++ src/service/ApiFoods.js
@@ -42,10 +42,3 @@ export function filterFoodButtons() {
     .then((data) => data.meals)
     .catch((error) => error);
 }
-
-export function detailApi(idReceita) {
-  return fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idReceita}`)
-    .then((response) => response.json())
-    .then((data) => data.meals)
-    .catch((error) => error);
-}
