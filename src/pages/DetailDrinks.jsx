5cb6c9728b3fe87a977760ff9884ba136d977a97
rbfelicino <rb.feliciano@hotmail.com>
2022-02-04 14:29:27 -0300

    Requisito 38

--- src/pages/DetailDrinks.jsx
+++ src/pages/DetailDrinks.jsx
@@ -98,15 +98,17 @@ function DetailDrinks({ match }) {
           )) }
         </div>
       ) }
-      <button
-        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold
-        hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent
-        rounded"
-        data-testid="start-recipe-btn"
-        type="button"
-      >
-        Start Recipe
-      </button>
+      <div>
+        <button
+          className="w-full fixed bottom-0 bg-blue-500 hover:bg-blue-500
+          text-white font-bold
+          py-2 border border-blue-700 rounded"
+          data-testid="start-recipe-btn"
+          type="button"
+        >
+          Start Recipe
+        </button>
+      </div>
     </div>
   );
 }
--- src/pages/DetailFoods.jsx
+++ src/pages/DetailFoods.jsx
@@ -103,16 +103,18 @@ function DetailFoods({ match }) {
           ))}
         </div>
       )}
-      <button
-        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold
-        hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent
-        rounded"
-        data-testid="start-recipe-btn"
-        type="button"
-        // onClick={ ()=> {} }
-      >
-        Start Recipe
-      </button>
+      <div>
+        <button
+          className="w-full fixed bottom-0 bg-blue-500 hover:bg-blue-700
+          text-white font-bold
+          py-2 border border-blue-700 rounded"
+          data-testid="start-recipe-btn"
+          type="button"
+          // onClick={ ()=> {} }
+        >
+          Start Recipe
+        </button>
+      </div>
     </div>
   );
 }
